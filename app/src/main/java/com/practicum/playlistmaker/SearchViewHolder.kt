4b811c02bfcab1c7e60d671d7673c3af19f0b33e
KitLeskinen <kawaiicreature@gmail.com>
2024-08-28 00:01:54 +0300

    Спринт 10

---
+++ app/src/main/java/com/practicum/playlistmaker/SearchViewHolder.kt
@@ -0,0 +1,24 @@
+package com.practicum.playlistmaker
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+
+class SearchViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    private val trackCover: ImageView = itemView.findViewById(R.id.trackCover)
+    private val trackName: TextView = itemView.findViewById(R.id.trackName)
+    private val artistName: TextView = itemView.findViewById(R.id.artistName)
+    private val trackTime: TextView = itemView.findViewById(R.id.trackTime)
+
+    fun bind(model: Track){
+        Glide.with(itemView).load(model.artworkUrl100).placeholder(R.drawable.placeholder).transform(RoundedCorners(Tools.dpToPx(2f, itemView.context))).into(trackCover)
+        trackName.text = model.trackName
+        artistName.text = model.artistName
+        trackTime.text = model.trackTime
+    }
+}
