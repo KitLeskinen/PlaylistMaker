0705cd4a391558151fa5c7233e3fc67906af9cc3
KitLeskinen <kawaiicreature@gmail.com>
2024-08-30 00:37:31 +0300

    Размеры перенесены в dimen, исправлены замечания

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -55,7 +55,8 @@ class SearchActivity : AppCompatActivity() {
 
         clearImageView.setOnClickListener() { view ->
             searchEditText.setText("")
-            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
             inputMethodManager?.hideSoftInputFromWindow(view.windowToken, 0)
         }
 
@@ -87,175 +88,135 @@ class SearchActivity : AppCompatActivity() {
         searchEditText.addTextChangedListener(textWatcher)
 
 
-        var mockArrayList: ArrayList<Track> = ArrayList();
-        mockArrayList.add(
+        var mockArrayList = listOf(
             Track(
                 "Smells Like Teen Spirit",
                 "Nirvana",
                 "5:01",
                 "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
-            )
-        )
-        mockArrayList.add(
+            ),
             Track(
                 "Test Long Song Name Test Long Song Name Test Long Song Name",
                 "Test Place Holder",
                 "5:01",
                 "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb_placeholder.jpg"
-            )
-        )
-        mockArrayList.add(
+            ),
             Track(
                 "Billie Jean",
                 "Michael Jackson",
                 "4:35",
                 "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
-            )
-        )
-        mockArrayList.add(
+            ),
             Track(
                 "Stayin' Alive",
                 "Bee Gees",
                 "4:10",
                 "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
-            )
-        )
-        mockArrayList.add(
+            ),
             Track(
                 "Whole Lotta Love",
                 "Led Zeppelin",
                 "5:33",
                 "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
-            )
-        )
-        mockArrayList.add(
+            ),
             Track(
                 "Sweet Child O'Mine",
                 "Guns N' Roses",
                 "5:03",
                 "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
-            )
-        )
-        mockArrayList.add(
+            ),
             Track(
                 "Smells Like Teen Spirit",
                 "Nirvana",
                 "5:01",
                 "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
-            )
-        )
-        mockArrayList.add(
+            ),
             Track(
                 "Billie Jean",
                 "Michael Jackson",
                 "4:35",
                 "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
-            )
-        )
-        mockArrayList.add(
+            ),
             Track(
                 "Stayin' Alive",
                 "Bee Gees",
                 "4:10",
                 "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
-            )
-        )
-        mockArrayList.add(
+            ),
             Track(
                 "Whole Lotta Love",
                 "Led Zeppelin",
                 "5:33",
                 "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
-            )
-        )
-        mockArrayList.add(
+            ),
             Track(
                 "Sweet Child O'Mine",
                 "Guns N' Roses",
                 "5:03",
                 "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
-            )
-        )
-        mockArrayList.add(
+            ),
             Track(
                 "Smells Like Teen Spirit",
                 "Nirvana",
                 "5:01",
                 "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
-            )
-        )
-        mockArrayList.add(
+            ),
             Track(
                 "Billie Jean",
                 "Michael Jackson",
                 "4:35",
                 "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
-            )
-        )
-        mockArrayList.add(
+            ),
             Track(
                 "Stayin' Alive",
                 "Bee Gees",
                 "4:10",
                 "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
-            )
-        )
-        mockArrayList.add(
+            ),
             Track(
                 "Whole Lotta Love",
                 "Led Zeppelin",
                 "5:33",
                 "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
-            )
-        )
-        mockArrayList.add(
+            ),
             Track(
                 "Sweet Child O'Mine",
                 "Guns N' Roses",
                 "5:03",
                 "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
-            )
-        )
-        mockArrayList.add(
+            ),
             Track(
                 "Smells Like Teen Spirit",
                 "Nirvana",
                 "5:01",
                 "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
-            )
-        )
-        mockArrayList.add(
+            ),
             Track(
                 "Billie Jean",
                 "Michael Jackson",
                 "4:35",
                 "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
-            )
-        )
-        mockArrayList.add(
+            ),
             Track(
                 "Stayin' Alive",
                 "Bee Gees",
                 "4:10",
                 "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
-            )
-        )
-        mockArrayList.add(
+            ),
             Track(
                 "Whole Lotta Love",
                 "Led Zeppelin",
                 "5:33",
                 "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
-            )
-        )
-        mockArrayList.add(
+            ),
             Track(
                 "Sweet Child O'Mine",
                 "Guns N' Roses",
                 "5:03",
                 "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
-            )
+            ),
         )
+
 
         val mockArrayListMerged = mockArrayList + mockArrayList + mockArrayList
 
--- app/src/main/java/com/practicum/playlistmaker/Tools.kt
+++ app/src/main/java/com/practicum/playlistmaker/Tools.kt
@@ -3,13 +3,11 @@ package com.practicum.playlistmaker
 import android.content.Context
 import android.util.TypedValue
 
-class Tools {
-    companion object{
+object Tools {
         fun dpToPx(dp: Float, context: Context): Int {
             return TypedValue.applyDimension(
                 TypedValue.COMPLEX_UNIT_DIP,
                 dp,
                 context.resources.displayMetrics).toInt()
         }
-    }
 }
